317d78597d196d79943ad2a80b76a11dd72d56b2
Nicolas MELONI <nicolas.meloni@synolia.com>
2020-01-09 09:44:20 +0100

    add humanizer to suggested packages

--- src/DependencyInjection/SchedulerCommandExtension.php
+++ src/DependencyInjection/SchedulerCommandExtension.php
@@ -4,13 +4,20 @@ declare(strict_types=1);
 
 namespace Synolia\SchedulerCommandPlugin\DependencyInjection;
 
+use Sivaschenko\Utility\Cron\ExpressionFactory;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-final class SchedulerCommandExtension extends Extension
+final class SchedulerCommandExtension extends Extension implements PrependExtensionInterface
 {
+    private const GRID_KEY = 'sylius_admin_scheduled_command';
+
+    /** @var array */
+    private $gridConf;
+
     /**
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
@@ -18,5 +25,47 @@ final class SchedulerCommandExtension extends Extension
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container): void
+    {
+        $this->gridConf = $container->getExtensionConfig('sylius_grid');
+
+        $this->removeHumanizedCronExpressionColumn($container);
+    }
+
+    private function removeHumanizedCronExpressionColumn(ContainerBuilder $container): void
+    {
+        if (class_exists(ExpressionFactory::class)) {
+            return;
+        }
+        $index = $this->findSchedulerCommandGridIndex();
+
+        unset($this->gridConf[$index]['grids'][self::GRID_KEY]['fields']['humanReadableExpression']);
+
+        $container->prependExtensionConfig(
+            'sylius_grid',
+            $this->gridConf[$index]
+        );
+    }
+
+    private function findSchedulerCommandGridIndex(): int
+    {
+        foreach ($this->gridConf as $key => $configuration) {
+            if (!isset($configuration['grids'])) {
+                continue;
+            }
+
+            if (!isset($configuration['grids'][self::GRID_KEY])) {
+                continue;
+            }
+
+            return $key;
+        }
+
+        throw new \LogicException('SchedulerCommand grid not found.');
     }
 }
