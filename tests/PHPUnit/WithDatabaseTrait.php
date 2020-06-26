b9ce19f05bf4211a6cd63eb26a2b2234b83cc6a0
Jibé Barth <jean-baptiste.barth@synolia.com>
2020-06-26 11:29:08 +0200

    Move ExecuteScheduleCommandTest Service in PHPUnit directory
    Create database trait to init database in memory

---
+++ tests/PHPUnit/WithDatabaseTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Synolia\SyliusSchedulerCommandPlugin\PHPUnit;
+
+use Doctrine\ORM\Tools\SchemaTool;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+trait WithDatabaseTrait
+{
+    public static function initDatabase(KernelInterface $kernel)
+    {
+        // Make sure we are in the test environment
+        if ('test' !== $kernel->getEnvironment()) {
+            throw new \LogicException('Primer must be executed in the test environment');
+        }
+
+        // Get the entity manager from the service container
+        $entityManager = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        // Run the schema update tool using our entity metadata
+        $metadatas = $entityManager->getMetadataFactory()->getAllMetadata();
+        $schemaTool = new SchemaTool($entityManager);
+        $schemaTool->updateSchema($metadatas);
+
+        // If you are using the Doctrine Fixtures Bundle you could load these here
+    }
+}
