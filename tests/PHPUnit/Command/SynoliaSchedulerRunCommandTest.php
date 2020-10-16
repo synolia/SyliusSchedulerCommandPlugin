46608be837dbe96200562b387c2c9d56cbf79934
hugo fesselier <hugo.fesselier@easycom.fr>
2020-10-16 13:12:13 +0200

    Fix bug and test it

--- tests/PHPUnit/Command/SynoliaSchedulerRunCommandTest.php
+++ tests/PHPUnit/Command/SynoliaSchedulerRunCommandTest.php
@@ -8,6 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 use Synolia\SyliusSchedulerCommandPlugin\Entity\ScheduledCommand;
+use Synolia\SyliusSchedulerCommandPlugin\Entity\ScheduledCommandInterface;
+use Synolia\SyliusSchedulerCommandPlugin\Repository\ScheduledCommandRepositoryInterface;
 use Tests\Synolia\SyliusSchedulerCommandPlugin\PHPUnit\WithDatabaseTrait;
 
 final class SynoliaSchedulerRunCommandTest extends KernelTestCase
@@ -76,6 +78,52 @@ final class SynoliaSchedulerRunCommandTest extends KernelTestCase
         self::assertStringContainsString('Immediately execution asked for : about', $commandTester->getDisplay());
         self::assertStringContainsString('Execute Command "about" - last execution : never', $commandTester->getDisplay());
         self::assertStringNotContainsString('Nothing to do', $commandTester->getDisplay());
+    }
+
+    /**
+     * @group toto
+     */
+    public function testExecuteNonExistentCommand(): void
+    {
+        $invalidCommandName = 'non:existent';
+
+        //remove last test
+        /** @var EntityManagerInterface $em */
+        $em = static::$container->get(EntityManagerInterface::class);
+        /** @var ScheduledCommandRepositoryInterface $repository */
+        $repository = $em->getRepository(ScheduledCommand::class);
+
+        $lastScheduledCommand = $repository->findOneBy(['command' => $invalidCommandName]);
+        if($lastScheduledCommand !== null) {
+            $repository->remove($lastScheduledCommand);
+        }
+
+
+        //generate command
+        /** @var ScheduledCommand $scheduledCommand */
+        $scheduledCommand = (new Factory(ScheduledCommand::class))->createNew();
+        $scheduledCommand
+            ->setCronExpression('* * * * *')
+            ->setCommand('non:existent');
+
+        $this->save($scheduledCommand);
+
+        //run command
+        $application = new Application(static::$kernel);
+
+        $command = $application->find(self::$commandName);
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([]);
+
+        //assertion
+        self::assertStringContainsString('Cannot find non:existent', $commandTester->getDisplay());
+
+        $persistedScheduledCommand = $repository->findOneBy(['command' => $invalidCommandName]);
+
+        self::assertEquals(
+            -1,
+            $persistedScheduledCommand->getLastReturnCode()
+        );
     }
 
     private function generateAboutScheduleCommand(string $cron): ScheduledCommand
