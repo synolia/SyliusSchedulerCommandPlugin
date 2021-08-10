aec372f3ecce5833fbd42326862c3aa1e1391467
Nicolas MELONI <nicolas.meloni@synolia.com>
2021-08-10 16:34:12 +0200

    fix migration namespace

--- src/Migrations/Version20210810054537.php
+++ src/Migrations/Version20210810054537.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Migrations;
+namespace Synolia\SyliusSchedulerCommandPlugin\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
