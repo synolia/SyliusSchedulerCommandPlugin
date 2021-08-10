733213111a7f8874dfb4d37c0a0bd5d06e5022e7
Nicolas MELONI <nicolas.meloni@synolia.com>
2021-08-10 16:13:52 +0200

    fixed phpspec

--- spec/Command/SynoliaSchedulerRunCommandSpec.php
+++ spec/Command/SynoliaSchedulerRunCommandSpec.php
@@ -1,22 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace spec\Synolia\SyliusSchedulerCommandPlugin\Command;
 
 use Doctrine\ORM\EntityManagerInterface;
 use PhpSpec\ObjectBehavior;
 use Symfony\Component\Console\Command\Command;
 use Synolia\SyliusSchedulerCommandPlugin\Command\SynoliaSchedulerRunCommand;
+use Synolia\SyliusSchedulerCommandPlugin\Repository\CommandRepositoryInterface;
 use Synolia\SyliusSchedulerCommandPlugin\Repository\ScheduledCommandRepositoryInterface;
 use Synolia\SyliusSchedulerCommandPlugin\Service\ExecuteScheduleCommand;
+use Synolia\SyliusSchedulerCommandPlugin\Service\ScheduledCommandPlanner;
 
 class SynoliaSchedulerRunCommandSpec extends ObjectBehavior
 {
     function let(
         EntityManagerInterface $scheduledCommandManager,
         ExecuteScheduleCommand $executeScheduleCommand,
-        ScheduledCommandRepositoryInterface $scheduledCommandRepository
+        CommandRepositoryInterface $commandRepository,
+        ScheduledCommandRepositoryInterface $scheduledCommandRepository,
+        ScheduledCommandPlanner $scheduledCommandPlanner
     ) {
-        $this->beConstructedWith("command", $scheduledCommandManager, $executeScheduleCommand, $scheduledCommandRepository);
+        $this->beConstructedWith($scheduledCommandManager, $executeScheduleCommand, $commandRepository, $scheduledCommandRepository, $scheduledCommandPlanner);
     }
 
     function it_is_initializable()
--- spec/Controller/DownloadControllerSpec.php
+++ spec/Controller/DownloadControllerSpec.php
@@ -1,16 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace spec\Synolia\SyliusSchedulerCommandPlugin\Controller;
 
 use PhpSpec\ObjectBehavior;
 use Symfony\Component\HttpFoundation\Response;
 use Synolia\SyliusSchedulerCommandPlugin\Controller\DownloadController;
 use Synolia\SyliusSchedulerCommandPlugin\Entity\ScheduledCommand;
-use Synolia\SyliusSchedulerCommandPlugin\Repository\ScheduledCommandRepository;
+use Synolia\SyliusSchedulerCommandPlugin\Repository\ScheduledCommandRepositoryInterface;
 
 class DownloadControllerSpec extends ObjectBehavior
 {
-    function let(ScheduledCommandRepository $scheduledCommandRepository)
+    function let(ScheduledCommandRepositoryInterface $scheduledCommandRepository)
     {
         $this->beConstructedWith($scheduledCommandRepository, 'logDir');
     }
@@ -20,11 +22,11 @@ class DownloadControllerSpec extends ObjectBehavior
         $this->shouldHaveType(DownloadController::class);
     }
 
-    function it_allows_to_download_a_log_file(ScheduledCommandRepository $scheduledCommandRepository)
+    function it_allows_to_download_a_log_file(ScheduledCommandRepositoryInterface $scheduledCommandRepository)
     {
-        $command = "about";
+        $command = 'about';
         $scheduledCommand = new ScheduledCommand();
-        $scheduledCommand->setName("Displays project information")
+        $scheduledCommand->setName('Displays project information')
             ->setCommand($command);
 
         $scheduledCommandRepository->find($command)->willReturn($scheduledCommand);
