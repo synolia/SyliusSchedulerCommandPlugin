e51241706d873109e4e2f1d14cd1340aa800d0cd
Nicolas MELONI <nicolas.meloni@synolia.com>
2021-08-13 11:51:24 +0200

    use name instead of command name for history display and better datetime format handler for executed At

---
+++ src/Grid/FieldType/DatetimeFieldType.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Synolia\SyliusSchedulerCommandPlugin\Grid\FieldType;
+
+use Sylius\Component\Grid\DataExtractor\DataExtractorInterface;
+use Sylius\Component\Grid\Definition\Field;
+use Sylius\Component\Grid\FieldTypes\FieldTypeInterface;
+use Sylius\Component\Locale\Context\LocaleContextInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Webmozart\Assert\Assert;
+
+final class DatetimeFieldType implements FieldTypeInterface
+{
+    /** @var DataExtractorInterface */
+    private $dataExtractor;
+
+    /** @var LocaleContextInterface */
+    private $localeContext;
+
+    public function __construct(DataExtractorInterface $dataExtractor, LocaleContextInterface $localeContext)
+    {
+        $this->dataExtractor = $dataExtractor;
+        $this->localeContext = $localeContext;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function render(Field $field, $data, array $options): string
+    {
+        $value = $this->dataExtractor->get($field, $data);
+        if (null === $value) {
+            return '';
+        }
+
+        $fmt = \datefmt_create($this->localeContext->getLocaleCode(), $options['date_format'], $options['time_format']);
+
+        Assert::isInstanceOf($value, \DateTimeInterface::class);
+        Assert::notFalse($fmt);
+
+        /** @phpstan-ignore-next-line  */
+        return $fmt->format($value) ?: '';
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'format' => 'Y-m-d H:i:s',
+            'date_format' => \IntlDateFormatter::SHORT,
+            'time_format' => \IntlDateFormatter::SHORT,
+        ]);
+        $resolver->setAllowedTypes('format', 'string');
+        $resolver->setAllowedTypes('date_format', 'integer');
+        $resolver->setAllowedTypes('time_format', 'integer');
+    }
+}
