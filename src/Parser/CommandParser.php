1a36a442b2de3b6183be000b45a5ffcd42d41932
Olivier ALLAIN <olivier.allain@synolia.com>
2023-09-09 16:23:37 +0200

    Better way to extract Command list

--- src/Parser/CommandParser.php
+++ src/Parser/CommandParser.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Synolia\SyliusSchedulerCommandPlugin\Parser;
 
 use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\StreamOutput;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Webmozart\Assert\Assert;
 
@@ -24,47 +22,16 @@ class CommandParser implements CommandParserInterface
     public function getCommands(): array
     {
         $application = new Application($this->kernel);
-        $application->setAutoExit(false);
 
-        $input = new ArrayInput(
-            [
-                'command' => 'list',
-                '--format' => 'json',
-            ],
-        );
-
-        $stream = fopen('php://memory', 'w+');
-
-        if ($stream === false) {
-            throw new \Exception('PHP Memory stream not available');
-        }
-
-        $output = new StreamOutput($stream);
-        $application->run($input, $output);
-        rewind($output->getStream());
-
-        return $this->extractCommandsFromJson((string) stream_get_contents($output->getStream()));
-    }
-
-    private function extractCommandsFromJson(string $string): array
-    {
-        if ($string === '') {
-            return [];
-        }
-
-        $node = \json_decode($string, null, 512, \JSON_THROW_ON_ERROR);
         $commandsList = [];
-
-        if (null === $node || !\is_array($node->namespaces)) {
-            return [];
-        }
-
-        foreach ($node->namespaces as $namespace) {
-            if (!in_array($namespace->id, $this->excludedNamespaces, true)) {
-                foreach ($namespace->commands as $command) {
-                    $commandsList[$namespace->id][$command] = $command;
-                }
+        $commands = $application->all();
+        foreach ($commands as $command) {
+            $name = $command->getName() ?? '';
+            $namespace = \explode(':', $name)[0];
+            if (in_array($namespace, $this->excludedNamespaces, true)) {
+                continue;
             }
+            $commandsList[$namespace][$command->getName()] = $name;
         }
 
         return $commandsList;
