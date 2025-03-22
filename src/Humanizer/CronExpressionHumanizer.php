dcbbfbe1b677bfeb9142980ab403f01653763859
Olivier ALLAIN <olivier.allain@synolia.com>
2025-03-22 19:09:29 +0100

    use php attribute

--- src/Humanizer/CronExpressionHumanizer.php
+++ src/Humanizer/CronExpressionHumanizer.php
@@ -7,12 +7,14 @@ namespace Synolia\SyliusSchedulerCommandPlugin\Humanizer;
 use Lorisleiva\CronTranslator\CronTranslator;
 use Sylius\Component\Locale\Context\LocaleContextInterface;
 use Sylius\Component\Locale\Context\LocaleNotFoundException;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class CronExpressionHumanizer implements HumanizerInterface
 {
     public function __construct(
         private readonly LocaleContextInterface $localeContext,
-        private readonly bool $timeFormat24Hours = false
+        #[Autowire(param: 'env(bool:SYNOLIA_SCHEDULER_PLUGIN_TIMEFORMAT_24H)')]
+        private readonly bool $timeFormat24Hours = false,
     ) {
     }
 
